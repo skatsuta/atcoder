200164565318a370cdb8a316bd64682093a6d6a8
Soshi Katsuta <soshi.katsuta@gmail.com>
2019-12-25 22:26:59 +0900

    abs: add ABC046B, ABC048B, ABC060B and ABC065B submissions

---
+++ abs/abc048b.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+
+sys.setrecursionlimit(10**7)
+
+# Override `input` function because `stdin.readline()` is 10x faster than built-in `input()`
+input = sys.stdin.readline
+
+
+def read_h(typ=int):
+    return list(map(typ, input().split()))
+
+
+def read_v(n, m=1, typ=int):
+    return [read_h(typ) if m > 1 else typ(input()) for _ in range(n)]
+
+
+def main():
+    a, b, x = read_h()
+    ans = b // x - (a - 1) // x + int(a % x == 0)
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
